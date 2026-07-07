a68231b93ceae4002b702460b94adbcd91e396a5
egokam <egokam.07@gmail.com>
2026-07-07 02:50:20 +0100

    footer section done.

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -56,16 +56,16 @@ const hobbies = [
     top: '5%',
   },
   {
-    title: 'Football',
-    emoji: '⚽',
-    left: '40%',
-    top: '40%',
-  },
-  {
     title: 'Painting',
     emoji: '🎨',
     left: '10%',
     top: '35%',
+  },
+  {
+    title: 'Football',
+    emoji: '⚽',
+    left: '40%',
+    top: '40%',
   },
   {
     title: 'Music',
@@ -117,7 +117,7 @@ export const AboutSection = () => {
           {/* here is my stop point 3:29:10 */}
 
 
-          <div className="grid grid-cols-1 md:grid-cols-5 gap-8 ">
+          <div className=" grid grid-cols-1 md:grid-cols-5 gap-8">
             <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
               <CardHeader title="Beyond the code" description="Explore my interests and hobbies beyond the digital realm" className="px-6 py-6" />
               <div className="relative flex-1 ">
@@ -133,7 +133,7 @@ export const AboutSection = () => {
               </div>
             </Card>
 
-            <Card className="h-[320px] p-0 relative col-span-2 lg:col-span-1">
+            <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
               <Image src={mapImage} alt="map" className="h-full w-full object-cover object-left-top" />
               <div className="absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30 ">
                 <Image src={smileMemoji} alt="smiling emoji" className="size-20" />
